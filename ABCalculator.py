1231a455a03e0e46086f8b67ff6181d1cbe053c1
EvgenyKochet <123293939+EvgenyKochet@users.noreply.github.com>
2023-03-18 20:27:23 +0500

    Создание файла программы калькулятора

---
+++ ABCalculator.py
@@ -0,0 +1,11 @@
+# A/B калькулятор
+
+import tkinter as tk
+
+# Окно главного окна
+root=tk.Tk()                                  # создание окна
+root.geometry("280x300")                      # задание размеров окна
+root.title("A/B калькулятор")                 # задание титульного названия окна  
+
+# Запуск цикла mainloop
+root.mainloop()
